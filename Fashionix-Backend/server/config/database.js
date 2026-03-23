5db4b6c412437b3aeb4f1cfa7f9f2445307e761a
Sahil23khanna <156312725+Sahil23khanna@users.noreply.github.com>
2026-03-23 12:23:59 +0530

    checking env

--- Fashionix-Backend/server/config/database.js
+++ Fashionix-Backend/server/config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-
+console.log("ENV CHECK:", process.env.MONGO_URL);
 mongoose.connect(process.env.MONGO_URL)         /* "mongodb://localhost:27017/NewCommerce" */
 
 .then(()=>{
